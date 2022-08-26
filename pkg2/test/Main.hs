b188cc2223a7526bea8debc58ddb84f2e48b5e29
Sirui Lu <siruilu@cs.washington.edu>
2022-08-26 13:09:32 -0700

    :test_tube: Test tasty

--- pkg2/test/Main.hs
+++ pkg2/test/Main.hs
@@ -1,7 +1,18 @@
 module Main where
 
-import Runner
-import qualified Spec
+import qualified Test.Tasty.Runners.Reporter as Reporter
+import Test.Tasty
+import Test.Tasty.HUnit
+
+tests = testGroup "Unit tests"
+  [ testCase "List comparison (different length)" $
+      [1, 2, 3] `compare` [1,2] @?= GT
+
+  -- the following test does not hold
+  , testCase "List comparison (same length)" $
+      [1, 2, 3] `compare` [1,2,2] @?= LT
+  ]
 
 main :: IO ()
-main = runTests "tests" Spec.spec
+main = defaultMainWithIngredients [Reporter.ingredient] tests
+
