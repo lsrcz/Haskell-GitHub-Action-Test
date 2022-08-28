3801fa376c971c28038ffdd267501c3025f90b1a
Sirui Lu <siruilu@cs.washington.edu>
2022-08-28 00:23:53 -0700

    :test_tube: Reorganize the tests

--- pkg1/test/Main.hs
+++ pkg1/test/Main.hs
@@ -1,16 +1,24 @@
 module Main where
-import qualified Test.Tasty.Runners.Reporter as Reporter
+
 import Test.Tasty
 import Test.Tasty.HUnit
+import qualified Test.Tasty.Runners.Reporter as Reporter
 
-tests = testGroup "Unit tests"
-  [ testCase "List comparison (different length)" $
-      [1, 2, 3] `compare` [1,2] @?= GT
-
-  -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= LT
-  ]
+tests =
+  testGroup
+    "Unit tests"
+    [ testGroup
+        "U1"
+        [ testCase "List comparison (different length)" $
+            [1, 2, 3] `compare` [1, 2] @?= GT
+        ],
+      -- the following test does not hold
+      testGroup
+        "U2"
+        [ testCase "List comparison (same length)" $
+            [1, 2, 3] `compare` [1, 2, 2] @?= LT
+        ]
+    ]
 
 main :: IO ()
 main = defaultMainWithIngredients [Reporter.ingredient] tests
--- pkg2/test/Main.hs
+++ pkg2/test/Main.hs
@@ -1,18 +1,18 @@
 module Main where
 
-import qualified Test.Tasty.Runners.Reporter as Reporter
 import Test.Tasty
 import Test.Tasty.HUnit
+import qualified Test.Tasty.Runners.Reporter as Reporter
 
-tests = testGroup "Unit tests"
-  [ testCase "List comparison (different length)" $
-      [1, 2, 3] `compare` [1,2] @?= GT
-
-  -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= LT
-  ]
+tests =
+  testGroup
+    "Unit tests"
+    [ testCase "List comparison (different length)" $
+        [1, 2, 3] `compare` [1, 2] @?= GT,
+      -- the following test does not hold
+      testCase "List comparison (same length)" $
+        [1, 2, 3] `compare` [1, 2, 2] @?= LT
+    ]
 
 main :: IO ()
 main = defaultMainWithIngredients [Reporter.ingredient] tests
-
